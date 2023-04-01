1c32bdf4d983998f337d53d360f396b851fa5ef6
Bossmaine <107197074+Bossmaine@users.noreply.github.com>
2023-04-01 08:02:30 +0100

    added the picture in picture fuctionality

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("testing")
+const getVideo = document.getElementById('video');
+const getButton = document.getElementById('button');
+
+async function startCapture(displayMediaOptions) {
+    try {
+      const captureStream = await navigator.mediaDevices.getDisplayMedia()
+        getVideo.srcObject = captureStream;
+        getVideo.onloadedmetadata = () => {
+            getVideo.play();
+        }
+      ;
+    } catch (err) {
+      alert(`Error: ${err}`);
+    }
+}
+
+getButton.addEventListener('click', async () => {
+    getButton.disabled = true;
+    await getVideo.requestPictureInPicture();
+    getButton.disabled; false;
+})  
+
+startCapture()
